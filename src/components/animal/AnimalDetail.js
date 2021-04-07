87e1d6366f48bec8741574e8c3b4a6c7173d2578
Dakota Upchurch <cheyenneupchurch@hotmail.com>
2021-04-07 10:52:13 -0500

    can now delete a location or animal from their respective details pages which then re routes to the respective list pages

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,35 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { getAnimalById } from '../../modules/AnimalManager';
+import { getAnimalById, deleteAnimal } from '../../modules/AnimalManager';
 import './AnimalDetail.css';
 import { useParams, useHistory } from "react-router-dom"
 
 export const AnimalDetail = () => {
-  const [animal, setAnimal] = useState({ name: "", breed: "" });
-
-  const {animalId} = useParams();
-  const history = useHistory();
-
-  useEffect(() => {
-    //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
-    console.log("useEffect", animalId)
-    getAnimalById(animalId)
-      .then(animal => {
-        setAnimal({
-          name: animal.name,
-          breed: animal.breed,
-          location: animal.location,
-          customer: animal.customer
+    const [animal, setAnimal] = useState({ name: "", breed: "" });
+    const [isLoading, setIsLoading] = useState(true);
+  
+    const {animalId} = useParams();
+    const history = useHistory();
+  
+    
+    useEffect(() => {
+        //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
+        console.log("useEffect", animalId)
+        getAnimalById(animalId)
+        .then(animal => {
+            setAnimal({
+                name: animal.name,
+                breed: animal.breed,
+                location: animal.location,
+                customer: animal.customer
+            });
+            setIsLoading(false);
         });
-      });
-  }, [animalId]);
+    }, [animalId]);
 
-  return (
-    <section className="animal">
-      <h3 className="animal__name">{animal.name}</h3>
-      <div className="animal__breed">{animal.breed}</div>
-      {/* What's up with the question mark???? See below.*/}
-      <div className="animal__location">Location: {animal.location?.name}</div>
-      <div className="animal__owner">Customer: {animal.customer?.name}</div>
-    </section>
-  );
-}
+    const handleDelete = () => {
+        //invoke the delete function in AnimalManger and re-direct to the animal list.
+        setIsLoading(true);
+        deleteAnimal(animalId).then(() =>
+          history.push("/animals")
+        );
+      };
+    
+    return (
+      <section className="animal">
+        <h3 className="animal__name">{animal.name}</h3>
+        <div className="animal__breed">{animal.breed}</div>
+        {/* What's up with the question mark???? See below.*/}
+        <div className="animal__location">Location: {animal.location?.name}</div>
+        <div className="animal__owner">Customer: {animal.customer?.name}</div>
+        <button type="button" disabled={isLoading} onClick={handleDelete}>
+            Discharge
+          </button>
+      </section>
+    );
+  };
--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { getLocationById } from '../../modules/LocationManager';
+import { getLocationById, deleteLocation } from '../../modules/LocationManager';
 import './LocationDetail.css';
 import { useParams, useHistory } from "react-router-dom"
 
 export const LocationDetail = () => {
   const [location, setLocation] = useState({ name: "", address: "" });
+    const [isLoading, setIsLoading] = useState(true);
 
   const {locationId} = useParams();
   const history = useHistory();
@@ -18,13 +19,23 @@ export const LocationDetail = () => {
           name: location.name,
           address: location.address
         });
+        setIsLoading(false);
       });
   }, [locationId]);
+
+  const handleDelete = () => {
+    //invoke the delete function in LocationManger and re-direct to the location list.
+    setIsLoading(true);
+    deleteLocation(locationId).then(() =>
+      history.push("/locations")
+    );
+  };
 
   return (
     <section className="location">
       <h3 className="location__name">{location.name}</h3>
       <div className="location__address">{location.address}</div>
+      <button type="button" disabled={isLoading} onClick={handleDelete}>Delete Location</button>
     </section>
   );
 }
